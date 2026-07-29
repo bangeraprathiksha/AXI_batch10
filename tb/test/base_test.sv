8d896f5eebff1afbed6a9887bca7b83a37bdb3fe
Preathiksha <109804004+bangeraprathiksha@users.noreply.github.com>
2026-07-29 19:04:49 +0530

    Add base_test class for UVM testing

---
+++ tb/test/base_test.sv
@@ -0,0 +1,32 @@
+class base_test extends uvm_test;
+ `uvm_component_utils(base_test)
+
+  top_env tenv;
+  sequence seq;
+
+
+ function new(string name="test", uvm_component parent);
+  super.new(name,parent);
+ endfunction 
+
+
+ function void build_phase(uvm_phase phase);
+  super.build_phase(phase);
+  tenv = environment::type_id::create("tenv", this);
+  seq = sequence::type_id::create("seq");
+ endfunction 
+
+ function void end_of_elaboration();
+ uvm_top.print_topology();
+ endfunction
+
+
+ task run_phase(uvm_phase phase);
+  phase.raise_objection(this);
+  if (!seq.randomize() with { num_txns == 10; })
+      `uvm_error(get_type_name(), "randomize failed")
+   seq.start(tenv.vseqr);
+ phase.drop_objection(this);
+ endtask  
+    
+endclass
