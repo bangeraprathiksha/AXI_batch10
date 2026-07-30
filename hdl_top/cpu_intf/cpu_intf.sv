2e500653f46d92fb678b86e17a652c6acc0f0ec0
Preathiksha <109804004+bangeraprathiksha@users.noreply.github.com>
2026-07-30 14:21:35 +0530

    Update cpu_intf.sv

--- hdl_top/cpu_intf/cpu_intf.sv
+++ hdl_top/cpu_intf/cpu_intf.sv
@@ -1,6 +1,4 @@
-interface cpu_intf(input bit clk);
-
-  logic rst;
+interface cpu_intf(input bit clk, input bit rst);
   logic wr_en;
   logic rd_en;
   logic [127:0] wr_data;
@@ -11,14 +9,14 @@ interface cpu_intf(input bit clk);
   // Driver Clocking Block
   clocking drv_cb @(posedge clk);
     default input #1 output #0;
-    output rst,wr_en,rd_en,wr_data;
+    output wr_en,rd_en,wr_data;
     input full,empty,rd_data;
   endclocking
 
   // Monitor Clocking Block
   clocking mon_cb @(posedge clk);
     default input #1 output #0;
-    input rst,wr_en,rd_en,wr_data,full,empty,rd_data;
+    input wr_en,rd_en,wr_data,full,empty,rd_data;
   endclocking
 
 endinterface
