abb494375438d72ffa3fd9757097f2589247b228
ShamaGowda <shamashivaram@gmail.com>
2026-07-30 15:11:51 +0530

    Update cpu_read_seq.sv

--- tb/active_agent/cpu_read_seq.sv
+++ tb/active_agent/cpu_read_seq.sv
@@ -14,54 +14,54 @@ class cpu_read_seq extends uvm_sequence #(axi_seq_item);
 
     start_item(req);
 
-    assert(req.randomize() with {
+    if (!req.randomize() with {
       txn_id inside {[0:15]};
-      addr[1:0] == 2'b00;      // Word aligned
+      addr[1:0] == 2'b00;
       burst inside {0,1,2};
       lock  inside {0,1};
       cache inside {[0:3]};
       prot  inside {[0:7]};
 
-      // Read packet contains only one DATA byte = 0
       data.size() == 1;
       data[0] == 32'h00000000;
 
       strobe.size() == 1;
       strobe[0] == 4'h0;
-    });
+    })
+      `uvm_fatal(get_type_name(), "Randomization Failed")
 
     finish_item(req);
-      `uvm_info(get_type_name(),
+
     `uvm_info(get_type_name(),
-      $sformatf("\n\
-=====================================================\n\
-CPU READ SEQUENCE GENERATED TRANSACTION\n\
------------------------------------------------------\n\
-TIME    : %0t\n\
-TXN_ID  : %0d\n\
-ADDR    : 0x%08h\n\
-LEN     : %0d\n\
-SIZE    : %0d\n\
-BURST   : %0d\n\
-LOCK    : %0d\n\
-CACHE   : %0d\n\
-PROT    : %0d\n\
-STROBE  : 0x%0h\n\
-DATA    : 0x%08h\n\
-=====================================================",
-        $time,
-        req.txn_id,
-        req.addr,
-        req.len,
-        req.size,
-        req.burst,
-        req.lock,
-        req.cache,
-        req.prot,
-        req.strobe[0],
-        req.data[0]),
+      $sformatf(
+      "\n=====================================================\n\
+       GENERATED READ TRANSACTION\n\
+       -----------------------------------------------------\n\
+       TIME    : %0t\n\
+       TXN_ID  : %0d\n\
+       ADDR    : 0x%08h\n\
+       LEN     : %0d\n\
+       SIZE    : %0d\n\
+       BURST   : %0d\n\
+       LOCK    : %0d\n\
+       CACHE   : %0d\n\
+       PROT    : %0d\n\
+       STROBE  : 0x%0h\n\
+       DATA    : 0x%08h\n\
+       =====================================================",
+      $time,
+      req.txn_id,
+      req.addr,
+      req.len,
+      req.size,
+      req.burst,
+      req.lock,
+      req.cache,
+      req.prot,
+      req.strobe[0],
+      req.data[0]),
       UVM_LOW)
-    `uvm_info(get_type_name(), "Generated Read Transaction", UVM_LOW)
+
     req.print();
 
   endtask
