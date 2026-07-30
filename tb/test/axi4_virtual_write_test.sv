8e9480d1c1ccf955cf25deda6c8bb92078342058
Preathiksha <109804004+bangeraprathiksha@users.noreply.github.com>
2026-07-30 12:49:01 +0530

    Update axi4_virtual_write_test.sv

--- tb/test/axi4_virtual_write_test.sv
+++ tb/test/axi4_virtual_write_test.sv
@@ -19,7 +19,7 @@ class axi4_virtual_write_test extends base_test;
     seq = axi4_virtual_write_seq::type_id::create("seq");
     if (!seq.randomize() with { num_txns == 10; })
       `uvm_error(get_type_name(), "randomize failed")
-      seq.start(tenv.vseqr);
+      seq.start(env_h.vseqr);
     phase.drop_objection(this);
   endtask
 
