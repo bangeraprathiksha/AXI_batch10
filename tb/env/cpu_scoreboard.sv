1b757496112112c0f83b665b18abe671e13de6cf
Preathiksha <109804004+bangeraprathiksha@users.noreply.github.com>
2026-07-30 12:54:16 +0530

    Update cpu_scoreboard.sv

--- tb/env/cpu_scoreboard.sv
+++ tb/env/cpu_scoreboard.sv
@@ -1,8 +1,8 @@
 class cpu_scoreboard extends uvm_scoreboard;
   `uvm_component_utils(cpu_scoreboard)
 
-  uvm_tlm_analysis_fifo #(cpu_seq_item) wrt_fifo;   // from write-fifo monitor
-  uvm_tlm_analysis_fifo #(cpu_seq_item) rd_fifo;   // from read-fifo monitor
+  uvm_tlm_analysis_fifo #(cpu_seq_item) cpu_imp;   // from write-fifo monitor
+  uvm_tlm_analysis_fifo #(cpu_seq_item) slave_imp;   // from read-fifo monitor
 
   function new(string name="cpu_scoreboard", uvm_component parent=null);
     super.new(name, parent);
@@ -10,8 +10,8 @@ class cpu_scoreboard extends uvm_scoreboard;
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    wrt_fifo = new("wrt_fifo", this);
-    rd_fifo = new("rd_mon_fifo", this);
+    cpu_imp = new("cpu_imp", this);
+    slave_imp = new("slave_imp", this);
   endfunction
 
   task run_phase(uvm_phase phase);
@@ -20,8 +20,8 @@ class cpu_scoreboard extends uvm_scoreboard;
       wrt = cpu_seq_item::type_id::create("wrt");
       rd  = cpu_seq_item::type_id::create("rd");
       fork
-      wrt_fifo.get(wrt);
-      rd_fifo.get(rd);
+      cpu_imp.get(wrt);
+      slave_imp.get(rd);
   join
   end
   endtask
