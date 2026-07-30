0505b8d8ece073a7bf33bd4a550e99f47297e5fa
ShamaGowda <shamashivaram@gmail.com>
2026-07-30 15:13:07 +0530

    Update cpu_passive_monitor.sv

--- tb/passive_agent/cpu_passive_monitor.sv
+++ tb/passive_agent/cpu_passive_monitor.sv
@@ -26,28 +26,26 @@ class cpu_passive_monitor extends uvm_monitor;
 
     forever begin
       @(vif.mon_cb);
+
       if (vif.rd_en && !vif.empty) begin
+
         item = cpu_seq_item::type_id::create("item");
         item.rd_data = vif.rd_data;
-         `uvm_info("PASSIVE_MON",
-        $sformatf("\n\
-=====================================================\n\
-PASSIVE MONITOR CAPTURED READ TRANSACTION\n\
------------------------------------------------------\n\
-TIME    : %0t\n\
-RD_EN   : %0b\n\
-EMPTY   : %0b\n\
-RD_DATA : 0x%08h\n\
-=====================================================",
-        $time,
-        vif.rd_en,
-        vif.empty,
-        item.rd_data),
-        UVM_LOW)
+
+        `uvm_info("PASSIVE_MON",
+          $sformatf("TIME=%0t RD_EN=%0b EMPTY=%0b RD_DATA=0x%08h",
+                    $time,
+                    vif.rd_en,
+                    vif.empty,
+                    item.rd_data),
+          UVM_LOW)
+
         passive_ap.write(item);
         passive_cg_port.write(item);
+
       end
     end
+
   endtask
 
 endclass
