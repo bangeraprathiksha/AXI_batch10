94a5a9bec80a68944b8c0040abf98594c59fccb6
Preathiksha <109804004+bangeraprathiksha@users.noreply.github.com>
2026-07-30 12:36:05 +0530

    Create write_seq.sv

---
+++ tb/active_agent/write_seq.sv
@@ -0,0 +1,33 @@
+class write_seq extends uvm_sequence #(axi_seq_item);
+
+  `uvm_object_utils(write_seq)
+
+  axi_seq_item req;
+
+  function new(string name = "write_seq");
+    super.new(name);
+  endfunction
+
+  task body();
+
+    req = axi_seq_item::type_id::create("req");
+
+    start_item(req);
+
+    assert(req.randomize() with {
+      txn_id inside {[0:15]};
+      addr[1:0] == 2'b00;      // Word aligned
+      burst inside {0,1,2};    // FIXED, INCR, WRAP
+      lock  inside {0,1};
+      cache inside {[0:3]};
+      prot  inside {[0:7]};
+    });
+
+    finish_item(req);
+
+    `uvm_info(get_type_name(), "Generated Write Transaction", UVM_LOW)
+    req.print();
+
+  endtask
+
+endclass
